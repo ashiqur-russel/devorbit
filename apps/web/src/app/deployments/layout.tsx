96f2cb475c3c9b4b02426d831f0fce56912aff2a
ashiqur-russel <ashiqur.rahman@afq-service.de>
2026-04-23 02:20:28 +0200

    fix (web): margin left and top removed on dashabord server stat

--- apps/web/src/app/deployments/layout.tsx
+++ apps/web/src/app/deployments/layout.tsx
@@ -7,7 +7,7 @@ export default function DeploymentsLayout({ children }: { children: React.ReactN
       <Sidebar />
       <div className="flex-1 flex flex-col ml-64">
         <TopBar />
-        <main className="flex-1 p-8 pt-24 overflow-y-auto">{children}</main>
+        <main className="flex-1 p-8 pt-2 overflow-y-auto">{children}</main>
       </div>
     </div>
   );
