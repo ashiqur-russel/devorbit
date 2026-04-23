f9e9658f1dc229c01d5e9cdf325cea62a367f682
ashiqur-russel <ashiqur.rahman@afq-service.de>
2026-04-23 02:37:24 +0200

    add: layout margin left chnage

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   return (
     <div className="flex min-h-screen bg-background">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col ml-64">
         <TopBar />
         <main className="flex-1 p-8 pt-24 overflow-y-auto">{children}</main>
       </div>
--- apps/web/src/app/servers/[id]/layout.tsx
+++ apps/web/src/app/servers/[id]/layout.tsx
@@ -5,7 +5,7 @@ export default function ServerDetailLayout({ children }: { children: React.React
   return (
     <div className="flex min-h-screen bg-background">
       <Sidebar />
-      <div className="flex-1 flex flex-col ml-64">
+      <div className="flex-1 flex flex-col ml-0">
         <TopBar />
         <main className="flex-1 p-8 pt-24 overflow-y-auto">{children}</main>
       </div>
